37acd89edb5a9750bd179f0a767c0dfdcec20b60
Yauheni Yurkevich <Yauheni.Yurkevich@secretsales.com>
2018-11-25 13:38:42 +0300

    add 3 test routes: index,json,cats

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -13,9 +14,26 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/json", name="json")
+     */
+    public function jsonAction()
+    {
+        return new JsonResponse([
+            'type' => 'json',
+            'key1' => 'test1',
+            'key2' => 'test2',
         ]);
+    }
+
+    /**
+     * @Route("/cats", name="cats")
+     */
+    public function catsAction()
+    {
+        return $this->render('default/cats.html.twig');
     }
 }
